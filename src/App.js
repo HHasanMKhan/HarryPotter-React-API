ab1e073580cafb0db36fbf1ff42a344deaf2cdc6
Hasan Khan <hasanmullerkhan@gmail.com>
2022-09-23 00:46:23 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [errorMsg, setErrorMsg] = useState('')
+  const [spells, setSpells] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setErrorMsg('');
+        const response = await fetch('https://wizard-world-api.herokuapp.com/Spells');
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        const data = await response.json();
+        console.log(data);
+        setSpells(data);
+      } catch (error) {
+        setErrorMsg("Oops, something went wrong...")
+        console.log(error.message);
+      }
+    }
+    fetchData();
+  }, [])
+
+  if (errorMsg !== '') {
+    return (
+      <h1>{errorMsg}</h1>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>harry potter</h1>
+      {spells.map((spell, index) => {
+        return (
+          <div key={index}>
+            <h1>{spell.name}</h1>
+            {spell.incantation == null ? (
+              <h2>{ `Incantation: There is no incantation for this spell` }</h2>
+              ) : <h2>{ `Incantation: ${spell.incantation}` }</h2>}
+            <h2>Effect: {spell.effect}</h2>
+          </div>
+        )
+      })}
     </div>
   );
 }
